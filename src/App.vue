06309478d61d7c6f5527dd51c2bf81b2767ec2c8
Perfavore8 <nekit2353@mail.ru>
2022-01-18 18:52:39 +0200

    first step

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <button
+  class="button1"
+  >1</button>
+  <button
+  class="button2"
+  >2</button>
+  <button
+  class="button3"
+  >3</button>
+  <button
+  class="button4"
+  >4</button>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+      line:[1,2,3,4]
+    }
+}
+</script>
+
+<style>
+
+.button1 {
+    background-color: rgba(255, 0, 0, 0.5);
+    color: black;
+}
+
+.button2 {
+    background-color: rgba(255, 255, 0, 0.5);
+    color: black;
+}
+
+.button3 {
+    background-color: rgba(0, 0, 255, 0.5);
+    color: black;
+}
+
+.button4 {
+    background-color: rgba(0, 128, 0, 0.5);
+    color: black;
+}
+
+</style>
